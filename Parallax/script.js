a7b5acc9616cde97d5a0cdbe02caa86002d2a0d7
Joel Lopes <joel95713@gmail.com>
2024-03-05 23:10:29 +0000

    boostrap

--- Parallax/script.js
+++
@@ -1,9 +0,0 @@
-var parallaxContainer = document.querySelector(".parallax-container");
-var parallaxBg = document.querySelector(".parallax-bg");
-var parallaxVideo = document.querySelector(".parallax-video");
-
-window.addEventListener("scroll", function () {
-  var scrolled = window.pageYOffset;
-  parallaxBg.style.top = -(scrolled * 0.3) + "px";
-  parallaxVideo.style.top = -(scrolled * 0.6) + "px";
-});
